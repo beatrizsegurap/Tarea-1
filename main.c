11b16a59d7b51a15436fefaf7474b4bc81bf9235
Leftzara <80830870+Leftzara@users.noreply.github.com>
2021-04-10 22:36:35 -0400

    Print para chekear donde falla

--- main.c
+++ main.c
@@ -581,7 +581,9 @@ List* crearHorario(List* B){
                 if(aux->lugares<5){
                 //Si el dia no esta completo lo añade altiro
                     strcpy(aux->name[aux->lugares],bomberoaux->Name);
+                    printf("%s",aux->name[aux->lugares]);
                     strcpy(aux->rut[aux->lugares],bomberoaux->Rut);
+                    printf("%s",aux->rut[aux->lugares]);
                     aux->disp[aux->lugares]=bomberoaux->diasDisp;
                     aux->lugares+=1;
                 }//------------------------------------------------------
